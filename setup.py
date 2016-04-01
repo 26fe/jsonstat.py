4932d288fce554355678edd62526f12a0dbe310a
26fe <gf@26fe.com>
2016-04-01 12:41:38 +0200

    setup.py reads dependencies from requirements.txt file

--- setup.py
+++ setup.py
@@ -1,14 +1,56 @@
 # stdlib
-# from builtins import exec
-
-import sys
 import os
+import re
 from setuptools import setup
+
 
 # version_file = open(os.path.join(mypackage_root_dir, 'VERSION'))
 # version = version_file.read().strip()
 
-exec(open('jsonstat/version.py').read())
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+exec (open('jsonstat/version.py').read())
+
+
+# cannot be able to import only version
+# because import jsonstat.version execute __init__
+# and not all dependencies are in place
+# from jsonstat.version import __version__
+
+# with open('requirements.txt') as f:
+#     install_requires = f.read().splitlines()
+#
+# with open('requirements_for_test.txt') as f:
+#     tests_require = f.read().splitlines()
+
+
+# From cburgmer/pdfserver
+# https://github.com/cburgmer/pdfserver/blob/master/setup.py
+def parse_requirements(file_name):
+    requirements = []
+    for line in open(file_name, 'r').read().split('\n'):
+        if re.match(r'(\s*#)|(\s*$)', line):
+            continue
+        if re.match(r'\s*-e\s+', line):
+            # TODO support version numbers
+            requirements.append(re.sub(r'\s*-e\s+.*#egg=(.*)$', r'\1', line))
+        elif re.match(r'\s*-f\s+', line):
+            pass
+        else:
+            requirements.append(line)
+
+    return requirements
+
+
+def parse_dependency_links(file_name):
+    dependency_links = []
+    for line in open(file_name, 'r').read().split('\n'):
+        if re.match(r'\s*-[ef]\s+', line):
+            dependency_links.append(re.sub(r'\s*-[ef]\s+', '', line))
+
+    return dependency_links
 
 
 # execfile is only python 2
@@ -18,21 +60,12 @@ exec(open('jsonstat/version.py').read())
 #     install_requires = ["threadpool >= 1.2.7"]
 # else:
 #     install_requires = ["threadpool >= 1.2.3"]
-#
-# setup(..., install_requires=install_requires)
-
-# from https://pythonhosted.org/an_example_pypi_project/setuptools.html
-# Utility function to read the README file.
-# Used for the long_description.
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
 
 setup(
     name="jsonstat.py",
     description='Library for reading JSON-stat format data.',
     long_description=read('README.rst'),
-    keywords="jsonstat json statistics",
+    keywords="jsonstat jsonstats json statistics",
 
     # https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
@@ -57,22 +90,22 @@ setup(
     author_email='gf@26fe.com',
 
     license='LGPL',
+
     # packages= setup.find_packages(exclude=['examples', 'docs', 'tests*', 'tmp']),
     packages=[
         'jsonstat', 'jsonstat.cli', 'istat'
     ],
 
-    install_requires=[
-        'pandas',
-        'requests',
-        'click'
-    ],
+    package_data={
+        'jsonstat': ['jsonschema_jsonstat.json'],
+    },
 
-    tests_require=[
-        'httpretty',
-        'sure',
-        'runipy'
-    ],
+    data_files=[('', ['requirements.txt'])],
+    include_package_data=True,
+
+    install_requires=parse_requirements('requirements.txt'),
+    dependency_links=parse_dependency_links('requirements.txt'),
+    tests_require=parse_requirements('requirements_for_test.txt'),
 
     entry_points={
         'console_scripts': [
