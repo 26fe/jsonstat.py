1d852729e91a8d6da433c1adbc9b2522a9743155
26fe <gf@26fe.com>
2016-05-02 23:16:31 +0200

    added local copies of jsonstat schema

---
+++ jsonstat/schemas/update_schema.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+curl -o jsonstat.json https://json-stat.org/format/schema/2.0/
+curl -O https://json-stat.org/format/schema/2.0/dataset.json
+curl -O https://json-stat.org/format/schema/2.0/collection.json
+curl -O https://json-stat.org/format/schema/2.0/dimension.json
+
