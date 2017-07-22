edb7b607ca2bcaf46fcb27718739c69346209cc8
26fe <gf@26fe.com>
2017-07-22 19:49:16 +0200

    schemas/*.json weren't included into sdist

--- setup.py
+++ setup.py
@@ -19,25 +19,19 @@ exec(open('jsonstat/version.py').read())
 # and not all dependencies are in place
 # from jsonstat.version import __version__
 
-# with open('requirements.txt') as f:
-#     install_requires = f.read().splitlines()
-#
-# with open('requirements_for_test.txt') as f:
-#     tests_require = f.read().splitlines()
-
 
 # From cburgmer/pdfserver
 # https://github.com/cburgmer/pdfserver/blob/master/setup.py
-def parse_requirements(file_name):
+def parse_requirements(fname):
     requirements = []
-    for line in open(file_name, 'r').read().split('\n'):
-        if re.match(r'\s*#', line): # comment
+    for line in open(os.path.join(os.path.dirname(__file__), fname)):
+        if re.match(r'\s*#', line):  # comment
             continue
-        if re.match(r'\s*$', line): # empty line
+        if re.match(r'\s*$', line):  # empty line
             continue
-        elif re.match(r'\s*-f\s+', line): # line starting with -f (find-links)
+        elif re.match(r'\s*-f\s+', line):  # line starting with -f (find-links)
             continue
-        elif re.match(r'\s*-r\s+', line): # line starting with -r (include requirements)
+        elif re.match(r'\s*-r\s+', line):  # line starting with -r (include requirements)
             continue
         if re.match(r'\s*-e\s+', line):
             # TODO support version numbers
@@ -48,9 +42,9 @@ def parse_requirements(file_name):
     return requirements
 
 
-def parse_dependency_links(file_name):
+def parse_dependency_links(fname):
     dependency_links = []
-    for line in open(file_name, 'r').read().split('\n'):
+    for line in open(os.path.join(os.path.dirname(__file__), fname)):
         if re.match(r'\s*-[ef]\s+', line):
             dependency_links.append(re.sub(r'\s*-[ef]\s+', '', line))
 
@@ -100,20 +94,20 @@ setup(
         'jsonstat', 'jsonstat.cli', 'istat'
     ],
 
+    include_package_data=True,
     package_data={
         'jsonstat': [
-            'schema/collection.json',
-            'schema/dataset.json',
-            'schema/dimension.json',
-            'schema/jsonstat.json',
+            'schemas/collection.json',
+            'schemas/dataset.json',
+            'schemas/dimension.json',
+            'schemas/jsonstat.json',
         ],
     },
 
     data_files=[('', ['requirements.txt'])],
-    include_package_data=True,
 
     install_requires=parse_requirements('requirements.txt'),
-    dependency_links=parse_dependency_links('requirements.txt'),
+    # dependency_links=parse_dependency_links('requirements.txt'),
     tests_require=parse_requirements('requirements_for_test.txt'),
 
     entry_points={
