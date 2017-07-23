16fc46f733ebe74c749e116e4834142be05b388c
26fe <gf@26fe.com>
2017-07-23 18:05:30 +0200

    readme format (try to fix formatting issue in pypi)

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@
 import os
 import re
 from setuptools import setup
+import re
+import sys, io
 
 
 # version_file = open(os.path.join(mypackage_root_dir, 'VERSION'))
@@ -53,6 +55,74 @@ def parse_dependency_links(fname):
     return dependency_links
 
 
+# https://stackoverflow.com/questions/16367770/my-rst-readme-is-not-formatted-on-pypi-python-org
+def yield_sphinx_only_markup(lines):
+    """
+    Cleans-up Sphinx-only constructs (ie from README.rst),
+    so that *PyPi* can format it properly.
+
+    To check for remaining errors, install ``sphinx`` and run::
+
+            python setup.py --long-description | sed -file 'this_file.sed' | rst2html.py  --halt=warning
+
+    :param file_inp:     a `filename` or ``sys.stdin``?
+    :param file_out:     a `filename` or ``sys.stdout`?`
+
+    """
+    substs = [
+        ## Selected Sphinx-only Roles.
+        #
+        (r':abbr:`([^`]+)`', r'\1'),
+        (r':ref:`([^`]+)`', r'`\1`_'),
+        (r':term:`([^`]+)`', r'**\1**'),
+        (r':dfn:`([^`]+)`', r'**\1**'),
+        (r':(samp|guilabel|menuselection):`([^`]+)`', r'``\2``'),
+
+        ## Sphinx-only roles:
+        #        :foo:`bar`   --> foo(``bar``)
+        #        :a:foo:`bar` XXX afoo(``bar``)
+        #
+        # (r'(:(\w+))?:(\w+):`([^`]*)`', r'\2\3(``\4``)'),
+        (r':(\w+):`([^`]*)`', r'\1(``\2``)'),
+
+        ## Sphinx-only Directives.
+        #
+        (r'\.\. doctest', r'code-block'),
+        (r'\.\. plot::', r'.. '),
+        (r'\.\. seealso', r'info'),
+        (r'\.\. glossary', r'rubric'),
+        (r'\.\. figure::', r'.. '),
+
+        ## Other
+        #
+        (r'\|version\|', r'x.x.x'),
+    ]
+
+    regex_subs = [(re.compile(regex, re.IGNORECASE), sub) for (regex, sub) in substs]
+
+    def clean_line(line):
+        regex, sub = (None, None)
+        try:
+            for (regex, sub) in regex_subs:
+                line = regex.sub(sub, line)
+        except Exception as ex:
+            print("ERROR: %s, (line(%s)" % (regex, sub))
+            raise ex
+
+        return line
+
+    for line in lines:
+        yield clean_line(line)
+
+
+def read_text_lines(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return f.readlines()
+
+
+readme_lines = read_text_lines('README.rst')
+long_desc = ''.join(yield_sphinx_only_markup(readme_lines))
+
 # execfile is only python 2
 # execfile('jsonstat/version.py')
 
@@ -64,7 +134,7 @@ def parse_dependency_links(fname):
 setup(
     name="jsonstat.py",
     description='Library for reading JSON-stat format data.',
-    long_description=read('README.rst'),
+    long_description=long_desc,
     keywords="jsonstat jsonstats json statistics",
 
     # https://pypi.python.org/pypi?%3Aaction=list_classifiers
