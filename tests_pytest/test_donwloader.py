0da50624f3da9f2d0983761da22725f6a440bf6c
26fe <gf@26fe.com>
2017-07-30 12:26:55 +0200

    moving from unittest to py.test

---
+++ tests_pytest/test_donwloader.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# This file is part of https://github.com/26fe/jsonstat.py
+# Copyright (C) 2016-2017 gf <gf@26fe.com>
+# See LICENSE file
+
+# stdlib
+from __future__ import print_function
+from __future__ import unicode_literals
+
+# external modules
+import pytest
+import requests
+import requests_mock
+
+# jsonstat
+import jsonstat.downloader
+import jsonstat
+
+
+# @httpretty.activate
+def test_downloader(tmpdir):
+    uri = 'http://json-stat.org/samples/oecd-canada.json'
+    body = 'This is a test'
+
+    with requests_mock.mock() as m:
+        m.get(uri, text=body)
+        d = jsonstat.Downloader(cache_dir=tmpdir)
+        response = d.download(uri)
+
+    assert body == response
