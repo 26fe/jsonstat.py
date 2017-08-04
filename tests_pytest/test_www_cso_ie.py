4727134dfa0f99839dda2d939b5c0640dda4efd2
26fe <gf@26fe.com>
2017-08-04 11:38:41 +0200

    moving from unittest to py.test

--- tests_pytest/test_www_cso_ie.py
+++ tests_pytest/test_www_cso_ie.py
@@ -7,49 +7,46 @@
 from __future__ import print_function
 from __future__ import unicode_literals
 import os
-import unittest
+
+# external lib
+import pytest
 
 # jsonstat
 import jsonstat
 
-
-class TestWWWCsoIe(unittest.TestCase):
-    def setUp(self):
-        self.fixture_dir = os.path.join(os.path.dirname(__file__), "fixtures", "www.cso.ie")
-
-    def test_parsing_NQQ25(self):
-        base_uri = 'http://www.cso.ie/StatbankServices/StatbankServices.svc/jsonservice/responseinstance/'
-        uri = base_uri + "NQQ25"
-        filename = "cso_ie-NQQ25.json"
-
-        json_string = jsonstat.download(uri, os.path.join(self.fixture_dir, filename))
-        collection = jsonstat.JsonStatCollection()
-        collection.from_string(json_string)
-
-        # extract dataset contained into collection
-        ds = collection.dataset(0)
-        self.assertIsNotNone(ds.dimension('Sector'))
-
-        data = ds.data(0)
-        self.assertEqual(19960, data.value)
-
-    def test_parsing_CIA01(self):
-        base_uri = 'http://www.cso.ie/StatbankServices/StatbankServices.svc/jsonservice/responseinstance/'
-        uri = base_uri + "CIA01"
-        filename = "cso_ie-CIA01.json"
-
-        json_string = jsonstat.download(uri, os.path.join(self.fixture_dir, filename))
-        collection = jsonstat.JsonStatCollection()
-        collection.from_string(json_string)
-
-        # extract dataset contained into collection
-        ds = collection.dataset(0)
-        # print(ds)
-        self.assertIsNotNone(ds.dimension('County and Region'))
-
-        data = ds.data(0)
-        self.assertEqual(41692, data.value)
+fixture_dir = os.path.join(os.path.dirname(__file__), "..", "tests", "fixtures", "www.cso.ie")
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_parsing_NQQ25():
+    base_uri = 'http://www.cso.ie/StatbankServices/StatbankServices.svc/jsonservice/responseinstance/'
+    uri = base_uri + "NQQ25"
+    filename = "cso_ie-NQQ25.json"
+
+    json_string = jsonstat.download(uri, os.path.join(fixture_dir, filename))
+    collection = jsonstat.JsonStatCollection()
+    collection.from_string(json_string)
+
+    # extract dataset contained into collection
+    ds = collection.dataset(0)
+    assert ds.dimension('Sector') is not None
+
+    data = ds.data(0)
+    assert 19960 == data.value
+
+
+def test_parsing_CIA01():
+    base_uri = 'http://www.cso.ie/StatbankServices/StatbankServices.svc/jsonservice/responseinstance/'
+    uri = base_uri + "CIA01"
+    filename = "cso_ie-CIA01.json"
+
+    json_string = jsonstat.download(uri, os.path.join(fixture_dir, filename))
+    collection = jsonstat.JsonStatCollection()
+    collection.from_string(json_string)
+
+    # extract dataset contained into collection
+    ds = collection.dataset(0)
+    # print(ds)
+    assert ds.dimension('County and Region') is not None
+
+    data = ds.data(0)
+    assert 41692 == data.value
