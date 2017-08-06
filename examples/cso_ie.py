7ed2b8d30e483b507f8d49563bb207c081fc8a00
26fe <gf@26fe.com>
2017-08-06 21:22:31 +0200

    moving from unittest to py.test

--- examples/cso_ie.py
+++ examples/cso_ie.py
@@ -30,7 +30,7 @@ def test(uri, filename):
 if __name__ == "__main__":
     # cache_dir directory where store json data downloaded from internet
     JSONSTAT_HOME = os.path.join(os.path.dirname(__file__), "..")
-    cache_dir = os.path.normpath(os.path.join(JSONSTAT_HOME, "tests", "fixtures", "www.cso.ie"))
+    cache_dir = os.path.normpath(os.path.join(JSONSTAT_HOME, "jsonstat-tests", "fixtures", "www.cso.ie"))
     jsonstat.cache_dir(cache_dir)
 
     base_uri = 'http://www.cso.ie/StatbankServices/StatbankServices.svc/jsonservice/responseinstance/'
